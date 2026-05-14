4d321c0ed311d2aa8e3c91ef73276e5178abe42d
ivan_maleshko <maleshko.ivan@student.uzhnu.edu.ua>
2026-05-14 14:47:54 +0300

    fix: home controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,7 @@ class HomeController < ApplicationController
 
   def index
     user_ids = current_user.followees.pluck(:id) << current_user.id
-    @posts = Post.where(user_id: user_ids).order(created_at: :desc)
+    @posts = Post.includes(:user).where(user_id: user_ids).order(created_at: :desc)
     @post = Post.new
   end
 end
